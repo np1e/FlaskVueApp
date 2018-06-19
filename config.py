ac564e8b6d8ed08b0d950b5780ec8ace730d1900
Nick Hafkemeyer <mail@nhafkemeyer.com>
2018-06-19 16:48:10 +0200

    added jwt secret key in config

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class DefaultConfig:
     # Basic Configuration
     SECRET_KEY = 'add_super-secret-key-here'
-
+    JWT_SECRET_KEY = 'secret-key'
     # Database Configuration
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///boilerplate.sqlite'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'

a816f9f6a6742bfa6ef0cf38140aa6c08f8397a7
Nick Hafkemeyer <mail@nhafkemeyer.com>
2018-06-23 10:12:43 +0200

    restructured navigation.js template

--- static/js/components/navigation.js
+++ static/js/components/navigation.js
@@ -16,32 +16,33 @@ Vue.component('navigation', {
     }
   },
   template: `
+      <nav class="navbar navbar-default">
         <div class="container-fluid">
-          <div class="navbar row">
-            <ul class="nav navbar-nav navbar-left" v-if='api.isLoggedIn'>
-                <li class="nav-item"><router-link to="/">Home</router-link></li>
-                <li class="nav-item"><router-link to="/profile/:id">My Profile</router-link></li>
-                <li class="nav-item"><a href="#" v-on:click='logout()'>Log Out</a></li>
-                <li class="nav-item"><router-link to="/post/create">Create post</router-link></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-left" v-else>
-                <li class="nav-item"><router-link to="/">Home</router-link></li>
-                <li class="nav-item"><router-link to="/login">Log In</router-link></li>
-                <li class="nav-item"><router-link to="/register">Register</router-link></li>
-            </ul>
-              <div v-if="api.isAdmin">
-                <li class="dropdown nav-item">
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <router-link to="/" class="navbar-brand" href="#">DefinitelyNotTwitter</router-link>
+              <ul class="nav navbar-nav">
+                <li v-if='api.isLoggedIn'><router-link to="/">Home</router-link></li>
+                <li v-if='api.isLoggedIn'><router-link :to="{name: 'user', params: {id: api.id}}">My Profile</router-link></li>
+                <li v-if='api.isLoggedIn'><router-link to="/post/create">Create post</router-link></li>
+                <li v-if='!api.isLoggedIn'><router-link to="/">Home</router-link></li>
+                <li v-if='!api.isLoggedIn'><router-link to="/login">Log In</router-link></li>
+                <li v-if='!api.isLoggedIn'><router-link to="/register">Register</router-link></li>
+                <li v-if='api.isAdmin' class="dropdown">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">Admin<span class="caret"></span></a>
                   <ul class="dropdown-menu">
                     <li><router-link to="/user-table">Users</router-link></li>
                     <li><router-link to="/admin-panel">Reviewpanel <span class="badge">5</span></router-link></li>
-
                   </ul>
                 </li>
-              </div>
-            </ul>
-            <!--<search-bar></search-bar>-->
+              </ul>
+              <search-bar></search-bar>
+              <ul class="nav navbar-nav navbar-right">
+                <li><span class="navbar-text">{{ api.username }}</span></li>
+                <li v-if='api.isLoggedIn'><a href="#" v-on:click='logout()'>Log Out</a></li>
+              </ul>
+
           </div>
         </div>
+      </nav>
   `
 })

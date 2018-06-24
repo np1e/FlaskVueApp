f5bff4d1f6834cebb6364cc1da55ab9cbd2e563a
Nick Hafkemeyer <mail@nhafkemeyer.com>
2018-06-24 13:52:01 +0200

    posts now displaying on user profile

--- static/js/components/post.js
+++ static/js/components/post.js
@@ -27,21 +27,21 @@ Vue.component("post", {
         this.imageURL = "/static/img/" + this.user.id;
       }
       return imageURL;
-    },
+    }
   },
   template: `
   <div class="post panel panel-default">
     <div class="panel-heading">
-      <img v-if="this.user.avatar === 1" :src="{ imgUrl }">
-      <img v-else src="/static/img/default.gif">
-      <router-link :to="{name: 'profile', params: {id: author.id}}"><h1>{{ author.username }}</h1></router-link>
+      <!--<img v-if="this.user.avatar === 1" :src="{ imgUrl }">
+      <img v-else src="/static/img/default.gif">-->
+      <router-link :to="{name: 'profile', params: {id: post.author_id}}"><h1>{{ post.author }}</h1></router-link>
       <p class="clearfix">{{ post.created.toString() }}</p>
       <a v-if="post.reviewed === 1" class="btn btn-success" v-on:click="releasePost(post.id)" href="#">Release</a>
       <a v-if="api.admin === 1" class="btn btn-danger" v-on:click="deletePost(post.id)" href="#">Delete</a>
-      <a v-else-if="api.id === author.id" class="btn btn-danger" v-on:click="deletePost(post.id)" href="#">Delete</a>
+      <a v-else-if="api.id === post.author_id" class="btn btn-danger" v-on:click="deletePost(post.id)" href="#">Delete</a>
     </div>
     <div class="panel-body">
-      { post.content }
+      {{ post.content }}
     </div>
   </div>
   `

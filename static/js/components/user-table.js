438f233a0917c77ee6a3d60997c1164b2dd47c3b
Nick Hafkemeyer <mail@nhafkemeyer.com>
2018-06-26 16:30:00 +0200

    user table now functional

--- static/js/components/user-table.js
+++ static/js/components/user-table.js
@@ -6,7 +6,8 @@
 Vue.component("user-table", {
   data() {
     return {
-      users: null
+      users: null,
+      error: ""
     };
   },
   mounted() {
@@ -17,27 +18,71 @@ Vue.component("user-table", {
       /** api.del kommt aus api-client.js */
       api.del(`/api/users/${userId}`, data => {
         this.fetchData()
-      })
+      });
     },
     fetchData() {
       /** api.get kommt aus api-client.js */
       api.get("/api/users", data => {
         this.users = data.users;
       });
+    },
+    promoteUser(userId) {
+      let user = null;
+      this.users.forEach(function(u) {
+        if(u.id == userId) {
+          user = u;
+        }
+      });
+      user.admin==1 ? user.admin=0 : user.admin=1;
+      console.log(user);
+      api.put(`/api/users/${userId}`,
+        user,
+        data => {
+          this.fetchData()
+        },
+        this.onError.bind(this)
+      );
+    },
+    onError(error) {
+      this.error = error.response.msg;
+    },
+    restrictUser(userId) {
+      let user = null;
+      this.users.forEach(function(u) {
+        if(u.id == userId) {
+          user = u;
+        }
+      });
+      user.restricted == 1 ? user.restricted = 0 : user.restricted = 1;
+      api.put(`/api/users/${userId}`,
+        user,
+        data => {
+          this.fetchData()
+        },
+        this.onError.bind(this)
+      );
     }
   },
   template: `
-    <table class="user-table">
+    <table class="table">
         <tr>
+            <th>ID</th>
             <th>Username</th>
+            <th># follower</th>
+            <th>Registered</th>
             <th>E-Mail</th>
             <th colspan="2">Tools</th>
         </tr>
         <tr v-for='user in users'>
-            <td>{{user.username}}</td>
+            <td>{{user.id}}</td>
+            <td >{{user.username}}</td>
+            <td>user.followers.length</td>
+            <td>{{user.registered}}</td>
             <td>{{user.email}}</td>
-            <td><router-link :to="{name: 'userEdit', params: {id: user.id}}" tag="button">edit</router-link></td>
-            <td><button type="button" @click="deleteUser(user.id)">delete</button></td>
+            <td><router-link class="btn" :to="{name: 'userEdit', params: {id: user.id}}" tag="button">edit</router-link></td>
+            <td><button class="btn" type="butto" @click="promoteUser(user.id)" v-bind:disabled="user.restricted == 1">{{ user.admin == 1 ? 'Strip' : 'Promote'}}</button></td>
+            <td><button class="btn" type="button" @click="restrictUser(user.id)" v-bind:disabled="user.admin == 1">{{ user.restricted == 1 ? 'Unrestrict' : 'Restrict'}}</button></td>
+            <td><button class="btn btn-danger" type="button" @click="deleteUser(user.id)">delete</button></td>
         </tr>
     </table>
     `
